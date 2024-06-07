09294ef72a6f34c251228cdc16335c67910b287b
Kenneth Haspeel <Kenneth.Haspeel@wervik.be>
2024-06-07 07:18:15 +0200

    authoptions in apart bestand

---
+++ app/api/auth/[...nextauth]/AuthOptions.ts
@@ -0,0 +1,82 @@
+import { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import * as bcrypt from "bcrypt";
+import db from "@/lib/prisma";
+
+export const authOptions: AuthOptions = {
+    pages:{
+      signIn: "/identity/login",
+    },
+      session: {
+        strategy: "jwt",
+      },
+      jwt: {
+        secret: process.env.NEXTAUTH_SECRET,
+      },
+      providers: [
+        CredentialsProvider({
+          name: "Credentials",
+          credentials: {
+            email: {
+              label: "Email",
+              type: "text",
+              placeholder: "john.doe@mail.com",
+            },
+            paswoord: { label: "Paswoord", type: "password" },
+          },
+          async authorize(credentials) {
+            const user = await db.user.findUnique({
+                include: {
+                  UserRoles: {
+                    include: {
+                      rol: {
+                        select: {
+                          rolnaam: true,
+                        },
+                      },
+                    },
+                  },
+                },
+                where: { email: credentials?.email },
+              });
+    
+            if (!user) throw new Error("Email of paswoord zijn niet correct");
+    
+            if (!credentials?.paswoord) throw new Error("Gelieve een paswoord in te geven");
+            const isPassowrdCorrect = await bcrypt.compare(credentials.paswoord, user.paswoord);
+    
+            if (!isPassowrdCorrect) throw new Error("User name or password is not correct");
+            let arr: string[] = [];
+            user.UserRoles.map((rol) => {
+              arr.push(rol.rol.rolnaam);
+              
+            });
+    
+            const u = {
+                id: user.id,
+                email: user.email,
+                voornaam: user.voornaam,
+                naam: user.naam,
+                rollen:arr
+            }
+            console.log(u)
+            return u;
+          },
+        }),
+      ],
+    
+      callbacks: {
+        async jwt({ token, user }) {
+          console.log(`Logging van callback: ${user}`)
+          if(user) token.user=user
+    
+          //if (user) token.rollen = user.rollen;
+          return token;
+        },
+    
+        async session({ token, session }) {
+            if (session?.user) session.user = token.user 
+            return session
+        },
+      },
+    };
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,86 +1,8 @@
-import db from "@/lib/prisma";
-import { AuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import * as bcrypt from "bcrypt";
+
+
 import NextAuth from "next-auth/next";
+import { authOptions } from "./AuthOptions";
 
-export const authOptions: AuthOptions = {
-pages:{
-  signIn: "/identity/login",
-},
-  session: {
-    strategy: "jwt",
-  },
-  jwt: {
-    secret: process.env.NEXTAUTH_SECRET,
-  },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-          placeholder: "john.doe@mail.com",
-        },
-        paswoord: { label: "Paswoord", type: "password" },
-      },
-      async authorize(credentials) {
-        const user = await db.user.findUnique({
-            include: {
-              UserRoles: {
-                include: {
-                  rol: {
-                    select: {
-                      rolnaam: true,
-                    },
-                  },
-                },
-              },
-            },
-            where: { email: credentials?.email },
-          });
-
-        if (!user) throw new Error("Email of paswoord zijn niet correct");
-
-        if (!credentials?.paswoord) throw new Error("Gelieve een paswoord in te geven");
-        const isPassowrdCorrect = await bcrypt.compare(credentials.paswoord, user.paswoord);
-
-        if (!isPassowrdCorrect) throw new Error("User name or password is not correct");
-        let arr: string[] = [];
-        user.UserRoles.map((rol) => {
-          arr.push(rol.rol.rolnaam);
-          
-        });
-
-        const u = {
-            id: user.id,
-            email: user.email,
-            voornaam: user.voornaam,
-            naam: user.naam,
-            rollen:arr
-        }
-        console.log(u)
-        return u;
-      },
-    }),
-  ],
-
-  callbacks: {
-    async jwt({ token, user }) {
-      console.log(`Logging van callback: ${user}`)
-      if(user) token.user=user
-
-      //if (user) token.rollen = user.rollen;
-      return token;
-    },
-
-    async session({ token, session }) {
-        if (session?.user) session.user = token.user 
-        return session
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 
