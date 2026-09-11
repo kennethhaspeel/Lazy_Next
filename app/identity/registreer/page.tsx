1286170336cb0e9f3416af35684f061773160978
kennethhaspeel <23094979+kennethhaspeel@users.noreply.github.com>
2026-09-11 10:36:27 +0200

    Refactor Registreer component structure

--- app/identity/registreer/page.tsx
+++ app/identity/registreer/page.tsx
@@ -6,8 +6,8 @@ import Image from "next/image";
 const Registreer = () => {
   return (
     <section className="pt-5 flex">
-      <div className="flex-1 p-4 flex flex-col  items-center">
-        {/*        <div className="text-xl font-bold mb-4">Registreer</div>
+     {/*    <div className="flex-1 p-4 flex flex-col  items-center">
+             <div className="text-xl font-bold mb-4">Registreer</div>
         <div className="w-full">
           <RegistreerForm />
         </div>
