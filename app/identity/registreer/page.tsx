3b529f46f475b86274363d22df909ba646c6bf88
kennethhaspeel <23094979+kennethhaspeel@users.noreply.github.com>
2026-09-11 10:31:44 +0200

    Update registration page layout and form visibility
    Uncomment registration form and adjust layout for larger screens.

--- app/identity/registreer/page.tsx
+++ app/identity/registreer/page.tsx
@@ -7,12 +7,13 @@ const Registreer = () => {
   return (
     <section className="pt-5 flex">
       <div className="flex-1 p-4 flex flex-col  items-center">
-        <div className="text-xl font-bold mb-4">Registreer</div>
+        {/*        <div className="text-xl font-bold mb-4">Registreer</div>
         <div className="w-full">
           <RegistreerForm />
         </div>
       </div>
-      <div className="hidden md:flex md:flex-1 md:max-w-full">
+      */}
+      <div className="md:flex md:flex-1 md:max-w-full">
         <Image
           src={Afbeelding}
           alt="loginafbeelding"
